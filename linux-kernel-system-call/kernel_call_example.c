7e00b93dcb51ad144d6228a3fd76a8007ce18c83
Dušan Jovanović <dusandjovanovic@outlook.com>
2020-01-18 18:33:55 +0100

    Update kernel_call_example.c

--- linux-kernel-system-call/kernel_call_example.c
+++ linux-kernel-system-call/kernel_call_example.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <linux/hernel.h>
-#include <sys/syscall.h>
 #include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
 
 #define __NR_sys_change_priority 436
-__syscall113(long, sys_change_priority, int, process_id, bool, process_higher_priority , bool, process_siblings, bool, process_realtime)
 
 int main() {
-	sys_change_priority(3117, true, true, false);
-	
+	long __return = syscall(436, 2620, 1, 1, 0);
+	printf("sys_call returned: %ld", __return);
 	return 0;
 }
